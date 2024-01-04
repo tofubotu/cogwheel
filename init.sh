f09767ca76109d2f86c2ba708b470214cd6ccac0
tofubotu <.>
2024-01-04 13:33:02 +0200

    init

---
+++ init.sh
@@ -0,0 +1,9 @@
+git init .
+git config user.name tofubotu
+git config user.email .
+git add test.txt
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:tofubotu/cogwheel.git
+git push -u origin main
+
