b5365cc4fee93a648ecb8edea8d00047cef5d37a
tofubotu <.>
2024-01-20 11:31:04 +0200

    .

--- build.bat
+++ build.bat
@@ -1,12 +1,13 @@
 set BUILD=c:\temp\build
-set PACKAGE=cogwheel-0.2-py3-none-any.whl
+set PACKAGE=cogwheel-0.1-py3-none-any.whl
 set SRC=c:\Dropbox\SYS\Cogwheel
-::rmdir /s /q %BUILD%
+rmdir /s /q %BUILD%
+
 mkdir %BUILD%
-robocopy %SRC% %BUILD%
+robocopy %SRC% %BUILD% /MIR
 pushd %BUILD%
 python -m build
-copy %BUILD%\dist\%PACKAGE% releases
 popd
+copy /y %BUILD%\dist\%PACKAGE% releases
 pip uninstall -y cogwheel
 pip install releases\%PACKAGE%
